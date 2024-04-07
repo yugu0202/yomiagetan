e061f9745679d8072a951d1af2c120511209a7f2
yugu-0202 <applerin1119@gmail.com>
2024-04-08 00:49:20 +0900

    ブラッシュアップ

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import json
 import discord
 from discord.ext import commands
 
@@ -8,12 +9,49 @@ from voicevox_core import VoicevoxCore
 intents = discord.Intents.all()
 bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
 
+json_path = Path("/settings.json")
+
 core = VoicevoxCore(open_jtalk_dict_dir=Path("./open_jtalk_dic_utf_8-1.11"))
 speaker_id = 68 #あいえるたん
 
 @bot.event
 async def on_ready():
   print(f"Logged in as {bot.user}")
+
+
+@bot.event
+async def on_message(message):
+  with open(json_path, "r") as f:
+    settings = json.load(f)
+
+  if message.channel.id in settings["track_channel_ids"]:
+    if message.author.id == bot.user.id:
+      return
+
+    if message.attachments:
+      for attachment in message.attachments:
+        if attachment.filename.endswith(".wav"):
+          return
+
+    if message.embeds:
+      for embed in message.embeds:
+        if embed.type == "video":
+          return
+
+    if message.guild.voice_client is None:
+      return
+
+    if not core.is_model_loaded(speaker_id):
+      core.load_model(speaker_id)
+    wave_bytes = core.tts(message.content, speaker_id)
+    with open("temp.wav", "wb") as f:
+      f.write(wave_bytes)
+    voice_client = message.guild.voice_client
+    voice_client.play(discord.FFmpegPCMAudio("temp.wav"))
+
+  await bot.process_commands(message)
+
+
 
 @bot.command()
 async def join(ctx):
@@ -23,6 +61,7 @@ async def join(ctx):
 
   await ctx.author.voice.channel.connect()
 
+
 @bot.command()
 async def leave(ctx):
   if ctx.voice_client is None:
@@ -30,6 +69,28 @@ async def leave(ctx):
     return
 
   await ctx.voice_client.disconnect()
+
+#特定のチャンネルのメッセージを追跡を設定する
+@bot.command()
+async def track(ctx, *, text: str):
+  print(text)
+  channel = discord.utils.get(ctx.guild.text_channels, id=int(text.strip('<#').strip('>')))
+  if channel is None:
+    await ctx.send(f"チャンネル {text} が見つかりません")
+    return
+
+  await ctx.send(f"{channel.name} を追跡します")
+
+  with open(json_path, "r") as f:
+    settings = json.load(f)
+
+  tci = settings["track_channel_ids"]
+  tci.append(channel.id)
+  settings["track_channel_ids"] = tci
+
+  with open(json_path, "w") as f:
+    json.dump(settings, f)
+
 
 @bot.command()
 async def tts(ctx, *, text: str):
@@ -44,5 +105,6 @@ async def tts(ctx, *, text: str):
     f.write(wave_bytes)
   voice_client = ctx.voice_client
   voice_client.play(discord.FFmpegPCMAudio("temp.wav"))
+
 
 bot.run(os.environ["DISCORD_BOT_TOKEN"])
